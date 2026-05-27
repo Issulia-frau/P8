40abe8d9e544afc5737c260a3bd16fe289baefb1
Issulia-frau <clement.delb17@gmail.com>
2026-05-27 21:47:55 +0200

    Add files via upload

--- test_api.py
+++ test_api.py
@@ -1,23 +1,84 @@
-from fastapi.testclient import TestClient
-from main import app
+import requests
 
-client = TestClient(app)
+BASE_URL = "http://localhost:8000"
 
 
 def test_healthcheck():
-    r = client.get("/")
-    assert r.status_code == 200
+    response = requests.get(f"{BASE_URL}/")
+
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
 
 
 def test_predict():
-    r = client.post("/predict", json={
-        "inputs": [{"feature1": 1, "feature2": 2, "feature3": 3}]
-    })
-    assert r.status_code == 200
+    payload = {
+        "inputs": [
+            {
+             "OWN_CAR_AGE0": 5,
+             "EXT_SOURCE_10": 0.45,
+             "EXT_SOURCE_30": 0.62,
+             "APARTMENTS_AVG0": 0.12,
+             "BASEMENTAREA_AVG0": 0.08,
+             "YEARS_BEGINEXPLUATATION_AVG0": 0.98,
+             "YEARS_BUILD_AVG0": 0.75,
+             "COMMONAREA_AVG0": 0.02,
+             "ELEVATORS_AVG0": 0.11,
+             "ENTRANCES_AVG0": 0.14,
+             "AMT_CREDIT_MAX_OVERDUE0": 1200,
+             "CNT_CREDIT_PROLONG0": 0,
+             "AMT_CREDIT_SUM0": 150000,
+             "AMT_CREDIT_SUM_DEBT0": 40000,
+             "AMT_CREDIT_SUM_LIMIT0": 200000
+            }
+        ]
+    }
+
+    response = requests.post(
+        f"{BASE_URL}/predict",
+        json=payload
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert "predictions" in body
+    assert "request_id" in body
+    assert "latency" in body
 
 
 def test_predict_opti():
-    r = client.post("/predictOpti", json={
-        "inputs": [{"feature1": 1, "feature2": 2, "feature3": 3}]
-    })
-    assert r.status_code == 200
+    payload = {
+        "inputs": [
+            {
+             "OWN_CAR_AGE0": 5,
+             "EXT_SOURCE_10": 0.45,
+             "EXT_SOURCE_30": 0.62,
+             "APARTMENTS_AVG0": 0.12,
+             "BASEMENTAREA_AVG0": 0.08,
+             "YEARS_BEGINEXPLUATATION_AVG0": 0.98,
+             "YEARS_BUILD_AVG0": 0.75,
+             "COMMONAREA_AVG0": 0.02,
+             "ELEVATORS_AVG0": 0.11,
+             "ENTRANCES_AVG0": 0.14,
+             "AMT_CREDIT_MAX_OVERDUE0": 1200,
+             "CNT_CREDIT_PROLONG0": 0,
+             "AMT_CREDIT_SUM0": 150000,
+             "AMT_CREDIT_SUM_DEBT0": 40000,
+             "AMT_CREDIT_SUM_LIMIT0": 200000
+            }
+        ]
+    }
+
+    response = requests.post(
+        f"{BASE_URL}/predictOpti",
+        json=payload
+    )
+
+    assert response.status_code == 200
+
+    body = response.json()
+
+    assert "predictions" in body
+    assert "request_id" in body
+    assert "latency" in body
